a06ffa2321fa9233ca4b9ebdc6736c685cd64823
kky0426 <ksakdlf99@naver.com>
2022-04-22 02:09:53 +0900

    Fix : 채팅 유실 문제 해결
    채팅방 find 후 message add 하는 방식에서 메시지 유실 발생 -> update/push 로 해결

--- src/main/java/com/bookpie/chatting/service/MessageListener.java
+++ src/main/java/com/bookpie/chatting/service/MessageListener.java
@@ -3,8 +3,13 @@ package com.bookpie.chatting.service;
 import com.bookpie.chatting.domain.ChattingRoom;
 import com.bookpie.chatting.domain.Message;
 import com.bookpie.chatting.repository.MessageRepository;
+import com.mongodb.client.result.UpdateResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -15,16 +20,20 @@ import org.springframework.stereotype.Component;
 public class MessageListener {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
-    private final MessageRepository messageRepository;
-
+    private final MongoTemplate mongoTemplate;
     @KafkaListener(topics = "kafka-chat",
                     groupId = "bookpie")
     private void listen(Message message){
         log.info("sending via kafka listener..");
         log.info(message.toString());
-        ChattingRoom room = messageRepository.findChattingRoomByTopic(message.getTopic());
-        room.addMessage(message);
-        messageRepository.save(room);
+
+        //ChattingRoom room = messageRepository.findChattingRoomByTopic(message.getTopic());
+        //room.addMessage(message);
+        //messageRepository.save(room);
+        mongoTemplate.updateFirst(
+                Query.query(Criteria.where("topic").is(message.getTopic())),
+                new Update().push("messages",message), ChattingRoom.class
+        );
         simpMessagingTemplate.convertAndSend("/topic/"+message.getTopic(),message);
     }
 
